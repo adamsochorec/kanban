267a6f81e7870612c318b7b3fe2f95401e4c5860
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-02-15 22:14:31 +0100

    Complete register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const user = require("../models/user");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 const { registrationValidation } = require("../validation");
 
 // registration
@@ -8,6 +9,29 @@ router.post("/register", async (req, res) => {
   const { error } = registrationValidation(req.body);
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
+  }
+
+  // check if the email is already registeted
+  const emailExist = await User.findOne({ email: req.body.email });
+
+  if (emailExist) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+
+  // has the password
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(req.body.password, salt);
+  const userObject = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password,
+  });
+
+  try {
+    const savedUser = await userObject.save();
+    res.json({ error: null, data: savedUser._id });
+  } catch (error) {
+    res.status(400).json({ error });
   }
 });
 
