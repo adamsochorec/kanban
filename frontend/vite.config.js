88bc61f7a816e5d3213951a35b3ed0e9ae734049
Adam S. <98590550+adamsochorec@users.noreply.github.com>
2025-06-01 00:39:48 +0200

    Dependabot vunerability fix V2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     vue(),
     Components({
       resolvers: [PrimeVueResolver()],
@@ -20,10 +20,14 @@ export default defineConfig({
     },
   },
   css: {
+    postcss: "./postcss.config.js",
     preprocessorOptions: {
       css: {
         additionalData: `@import "./src/assets/style.css";`,
       },
     },
+  },
+  optimizeDeps: {
+    exclude: ["autoprefixer"],
   },
 });
